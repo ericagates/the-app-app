ca41b7f474da1b5ac7bd9388630e88fc32a333ef
erica gates <ericajoangates@gmail.com>
2019-03-13 00:26:38 -0700

    my first App App commit

---
+++ src/the-app-app.py
@@ -0,0 +1,9 @@
+# Project Name: The App App
+# The purpose of this project is to create an appointment booking app for
+# companies that provide services such as a salon, a personal trainer, or a
+# personal driver. etc. It would allow the service provider to enter their
+# availability in a calendar and allow customers to schedule appointments for
+# services. The app would then send either an email or a text notification to
+# the service provider that an appointment has been booked.
+
+print('Hello, World!')
